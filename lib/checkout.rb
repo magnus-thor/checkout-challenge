3c02cfbb25567049a14554d1bcbc030fc05f76e6
magnus-thor <magnus.thor.ar@gmail.com>
2017-08-30 16:17:19 +0200

    can add items to basket

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,12 +1,17 @@
 class Checkout
 
-  attr_accessor :inventory
+  attr_accessor :inventory, :basket
 
   def initialize
     @inventory = set_inventory
+    @basket = []
   end
 
   def set_inventory
-    {1=> {'tie'=> 9.25}, 2=> {'Sweater'=> 45.0}, 3=> {'skirt'=> 19.95}}
+    {1=> ['tie', 9.25], 2=> ['Sweater', 45.0], 3=> ['skirt', 19.95]}
+  end
+
+  def scan(item)
+    basket << item
   end
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -5,7 +5,18 @@ describe Checkout do
   subject { described_class.new() }
 
   it 'has inventory on initialize' do
-    expected_output = {1=> {'tie'=> 9.25}, 2=> {'Sweater'=> 45.0}, 3=> {'skirt'=> 19.95}}
+    expected_output = {1=> ['tie', 9.25], 2=> ['Sweater', 45.0], 3=> ['skirt', 19.95]}
     expect(subject.inventory).to eq expected_output
+  end
+
+  it 'has empty basket on initialize' do
+    expect(subject.basket).not_to be nil
+  end
+
+  it 'adds items to basket' do
+    expected_output = [1, 2]
+    subject.scan(1)
+    subject.scan(2)
+    expect(subject.basket).to eq expected_output
   end
 end
