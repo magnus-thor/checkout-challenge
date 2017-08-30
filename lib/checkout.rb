bf1964e1a8243a0277ed1a28a8d46895804c77cc
magnus-thor <magnus.thor.ar@gmail.com>
2017-08-30 16:00:41 +0200

    has inventory on initialize

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,6 +1,12 @@
 class Checkout
 
+  attr_accessor :inventory
 
+  def initialize
+    @inventory = set_inventory
+  end
 
-
+  def set_inventory
+    {1=> {'tie'=> 9.25}, 2=> {'Sweater'=> 45.0}, 3=> {'skirt'=> 19.95}}
+  end
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -2,5 +2,10 @@ require './lib/checkout.rb'
 
 describe Checkout do
 
+  subject { described_class.new() }
 
+  it 'has inventory on initialize' do
+    expected_output = {1=> {'tie'=> 9.25}, 2=> {'Sweater'=> 45.0}, 3=> {'skirt'=> 19.95}}
+    expect(subject.inventory).to eq expected_output
+  end
 end
